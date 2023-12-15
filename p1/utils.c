26b8470a9ce6069e9acb54609b6a543ccd0f6a93
Inês Paredes <ines.paredes@tecnico.ulisboa.pt>
2023-12-15 15:19:36 +0000

    Colocação de comentarios

--- p1/utils.c
+++ p1/utils.c
@@ -2,9 +2,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/**
- * Initializes a rwlock. When unsuccessful, it exits with failure.
- */
 void rwlock_init(pthread_rwlock_t *lock) {
   if (pthread_rwlock_init(lock, NULL) != 0) {
     perror("Failed to initalize the rwlock");
@@ -12,9 +9,6 @@ void rwlock_init(pthread_rwlock_t *lock) {
   }
 }
 
-/**
- * Destroys a rwlock. When unsuccessful, it exits with failure.
- */
 void rwlock_destroy(pthread_rwlock_t *lock) {
   if (pthread_rwlock_destroy(lock) != 0) {
     perror("Failed to destroy the rwlock");
@@ -22,29 +16,20 @@ void rwlock_destroy(pthread_rwlock_t *lock) {
   }
 }
 
-/**
- * Locks a rwlock to read-only. When unsuccessful, it exits with failure.
- */
 void rwlock_rdlock(pthread_rwlock_t *lock) {
   if (pthread_rwlock_rdlock(lock) != 0) {
-    perror("Failed to lock the rwlock to read-only");
+    perror("Failed to lock the read-only rwlock");
     exit(EXIT_FAILURE);
   }
 }
 
-/**
- * Locks a rwlock to write-read. When unsuccessful, it exits with failure.
- */
 void rwlock_wrlock(pthread_rwlock_t *lock) {
   if (pthread_rwlock_wrlock(lock) != 0) {
-    perror("Failed to lock the rwlock to write-read");
+    perror("Failed to lock the write-read rwlock");
     exit(EXIT_FAILURE);
   }
 }
 
-/**
- * Unlocks a rwlock. When unsuccessful, it exits with failure.
- */
 void rwlock_unlock(pthread_rwlock_t *lock) {
   if (pthread_rwlock_unlock(lock) != 0) {
     perror("Failed to unlock the rwlock");
@@ -52,9 +37,6 @@ void rwlock_unlock(pthread_rwlock_t *lock) {
   }
 }
 
-/**
- * Initializes a mutex. When unsuccessful, it exits with failure.
- */
 void mutex_init(pthread_mutex_t *mutex) {
   if (pthread_mutex_init(mutex, NULL) != 0) {
     perror("Failed to Initialize the mutex");
@@ -62,9 +44,6 @@ void mutex_init(pthread_mutex_t *mutex) {
   }
 }
 
-/**
- * Destroys a mutex. When unsuccessful, it exits with failure.
- */
 void mutex_destroy(pthread_mutex_t *mutex) {
   if (pthread_mutex_destroy(mutex) != 0) {
     perror("Failed to destroy the mutex");
@@ -72,9 +51,6 @@ void mutex_destroy(pthread_mutex_t *mutex) {
   }
 }
 
-/**
- * Locks a mutex. When unsuccessful, it exits with failure.
- */
 void mutex_lock(pthread_mutex_t *mutex) {
   if (pthread_mutex_lock(mutex) != 0) {
     perror("Failed to lock the mutex");
@@ -82,9 +58,6 @@ void mutex_lock(pthread_mutex_t *mutex) {
   }
 }
 
-/**
- * Unlocks a mutex. When unsuccessful, it exits with failure.
- */
 void mutex_unlock(pthread_mutex_t *mutex) {
   if (pthread_mutex_unlock(mutex) != 0) {
     perror("Failed to unlock the mutex");
--- p1/utils.h
+++ p1/utils.h
@@ -3,15 +3,58 @@
 
 #include <pthread.h>
 
+/*
+ * Initializes the rwlock. Exits if the initialization fails.
+ * @param lock  The rwlock to be initialized.
+ */
 void rwlock_init(pthread_rwlock_t *lock);
+
+/*
+ * Destroys the rwlock. Exits if the destruction fails.
+ * @param lock  The rwlock to be destroyed.
+ */
 void rwlock_destroy(pthread_rwlock_t *lock);
+
+/*
+ *Locks the rwlock for reading. Exits if the lock fails.
+ * @param lock  The rwlock to be locked.
+ */
 void rwlock_rdlock(pthread_rwlock_t *lock);
+
+/*
+ *Locks the rwlock for writing. Exits if the lock fails. 
+ * @param lock  The rwlock to be locked.
+ */
 void rwlock_wrlock(pthread_rwlock_t *lock);
+
+/*
+ *Unlocks the rwlock. Exits if the unlock fails.
+ * @param lock  The rwlock to be unlocked.
+ */
 void rwlock_unlock(pthread_rwlock_t *lock);
 
+/*
+ * Initializes the mutex. Exits if the initialization fails.
+ * @param mutex  The mutex to be initialized.
+ */
 void mutex_init(pthread_mutex_t *mutex);
+
+/*
+ * Destroys the mutex. Exits if the destruction fails.
+ * @param mutex  The mutex to be destroyed.
+ */
 void mutex_destroy(pthread_mutex_t *mutex);
+
+/*
+ *Locks the mutex. Exits if the lock fails.
+ * @param mutex  The mutex to be locked. 
+ */
 void mutex_lock(pthread_mutex_t *mutex);
+
+/*
+ *Unlocks the mutex. Exits if the unlock fails. 
+ * @param mutex  The mutex to be unlocked.
+ */
 void mutex_unlock(pthread_mutex_t *mutex);
 
 #endif // UTILS_H
